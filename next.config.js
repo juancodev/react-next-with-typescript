1a49d083b360ff2319c1ace1b8983ed1b0847452
juancodev <montillasanchezjuancarlos@gmail.com>
2023-03-18 00:21:57 -0400

    [⚡ ADD] New project with react and deep concepts of typescript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'randomfox.ca'
+    }]
+  }
 }
 
 module.exports = nextConfig
---
+++ src/components/RandomFox.tsx
@@ -0,0 +1,27 @@
+// import type { FunctionComponent, FC } from "react";
+import Image from "next/image";
+
+const randomNumber = (): number => {
+  let valueFinal: number = 123;
+
+  const resultRandom: number = Math.floor(Math.random() * valueFinal);
+  return resultRandom;
+};
+
+export const RandomFox = (): JSX.Element => {
+  const image: string = `https://randomfox.ca/images/${randomNumber()}.jpg`;
+
+  return (
+    <Image src={image} alt="" width={320} height={320} className={"rounded"} />
+  );
+};
+
+// Helper of React
+
+// export const RandomFox = (): FunctionComponent => {
+//   return <img src="" alt="" />;
+// };
+
+// export const RandomFox: FC = () => {
+//   return <img src="" alt="" />;
+// };
