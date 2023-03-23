3f0363b74caa12c707ce90c86220d05612a0e67c
juancodev <montillasanchezjuancarlos@gmail.com>
2023-03-23 01:07:42 -0400

    [⚡ ADD] Lazy images with observable

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,18 +1,47 @@
 // import type { FunctionComponent, FC } from "react";
 import Image from "next/image";
+import { useRef, useEffect, useState } from "react";
 
 type Props = {
   image: string;
 };
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
+  const node = useRef<HTMLImageElement>(null);
+  const [src, setSrc] = useState(
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  );
+
+  useEffect(() => {
+    // new observable
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        // onIntersection
+        if (entry.isIntersecting) {
+          setSrc(image);
+        }
+      });
+    });
+
+    //observe node
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    //disconnect
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
+
   return (
     <Image
-      src={image}
+      src={src}
+      ref={node}
       alt="Fox Image"
       width={320}
       height={320}
-      className={"rounded"}
+      className={"rounded bg-gray-300"}
     />
   );
 };
